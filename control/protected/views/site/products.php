1b5544eed0673025fc7ad20dcfcd6d5c560c97a1
claudericke <you@example.com>
2015-05-14 11:15:58 +0200

    Style changes to Products views and Services Views

--- control/protected/views/site/products.php
+++ control/protected/views/site/products.php
@@ -4,22 +4,7 @@
     <h2>Products</h2>
 </div>
 <style type="text/css">
-    .productDescription,
-    .ProductName {
-        display: block;
-        float: left;
-        clear: right;
-        width: 250px;
-        line-height: 25px;
-        min-height: 60px;
-        margin: 4px;
-        background-color: #EEE;
-        padding: 2px;
-    }
 
-    .productDescription {
-        width: 350px;
-    }
 </style>
 
 <a href="/control/site/addproduct">Add a product</a>
@@ -28,9 +13,9 @@
 if (count($aProducts) > 0) {
     foreach ($aProducts as $aProduct) {
         ?>
-        <div class="row">
-            <div class="ProductName"><a href="/control/site/addproduct/?pid=<?php echo $aProduct["id"]; ?>" title="<?php echo $aProduct["ProductName"]; ?>"><?php echo $aProduct["ProductName"]; ?></a></div>
-            <div class="productDescription"><?php echo $aProduct["Description"]; ?></div>
+        <div class="row single-item">
+            <div class="itemName"><a href="/control/site/addproduct/?pid=<?php echo $aProduct["id"]; ?>" title="<?php echo $aProduct["ProductName"]; ?>"><?php echo $aProduct["ProductName"]; ?></a></div>
+            <div class="itemDescription"><?php echo $aProduct["Description"]; ?></div>
             <br style="clear: both"/>
         </div>
         <?php
--- control/protected/views/site/services.php
+++ control/protected/views/site/services.php
@@ -3,24 +3,6 @@
 <div class="row">
     <h2>Services</h2>
 </div>
-<style type="text/css">
-    .productDescription,
-    .ProductName {
-        display: block;
-        float: left;
-        clear: right;
-        width: 250px;
-        line-height: 25px;
-        min-height: 60px;
-        margin: 4px;
-        background-color: #EEE;
-        padding: 2px;
-    }
-
-    .productDescription {
-        width: 350px;
-    }
-</style>
 
 <a href="/control/site/addservice">Add a service</a>
 <br style="clear: both"/>
@@ -28,9 +10,9 @@
 if (count($aServices) > 0) {
     foreach ($aServices as $aService) {
         ?>
-        <div class="row">
-            <div class="ProductName"><a href="/control/site/addservice/?pid=<?php echo $aService["id"]; ?>" title="<?php echo $aService["Service"]; ?>"><?php echo $aService["Service"]; ?></a></div>
-            <div class="productDescription"><?php echo $aService["Description"]; ?></div>
+        <div class="row single-item">
+            <div class="itemName six columns"><a href="/control/site/addservice/?pid=<?php echo $aService["id"]; ?>" title="<?php echo $aService["Service"]; ?>"><?php echo $aService["Service"]; ?></a></div>
+            <div class="itemDescription six columns"><?php echo $aService["Description"]; ?></div>
             <br style="clear: both"/>
         </div>
         <?php
